1c3d1aaa970ca02d50ec4e0cd772b9c2210c6acc
Donald <donald@drmer.net>
2018-12-30 22:21:00 +0800

    add uuid validation rule

--- src/MigrationServiceProvider.php
+++ src/MigrationServiceProvider.php
@@ -5,6 +5,8 @@ namespace Drmer\Laravel\Migration;
 use Illuminate\Support\ServiceProvider;
 use Drmer\Laravel\Migration\Support\StrMigration;
 use Drmer\Laravel\Migration\Routing\UrlGeneratorMigration;
+use Ramsey\Uuid\Uuid;
+use Illuminate\Support\Facades\Validator;
 use Drmer\Laravel\Migration\Foundation\Testing\TestResponseMigration;
 
 class MigrationServiceProvider extends ServiceProvider
@@ -16,7 +18,9 @@ class MigrationServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('uuid', function ($attribute, $value, $parameters, $validator) {
+            return Uuid::isValid($value);
+        });
     }
 
     /**
